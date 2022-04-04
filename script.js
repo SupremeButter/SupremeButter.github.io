a59c53b1f36cab6b5cf190b184df2827cdcc853d
SupremeButter <83093090+SupremeButter@users.noreply.github.com>
2022-04-04 16:24:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js"></script>
-<script>iFrameResize({ log: false, minHeight: 360 }, '#buttonApi')</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js"></script>
-<script>iFrameResize({ log: false, minHeight: 400 }, '#widgetApi')</script>
+
